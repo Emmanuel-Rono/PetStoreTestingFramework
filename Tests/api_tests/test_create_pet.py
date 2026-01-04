d095a8faee5b024847617607fc44111ce7abe746
Emmanuel_Rono <ronoemmanuel933@gmail.com>
2026-01-04 17:19:46 +0300

    wrote assertions

--- Tests/api_tests/test_create_pet.py
+++ Tests/api_tests/test_create_pet.py
@@ -1,12 +1,14 @@
 import pytest
 
-from Tests.conftest import pet_service
+from Tests.conftest import pet_service, valid_pet_payload
 
 
-def test_create_pet(pet_service):
-    create_pet_response = pet_service.add_pet()
-    assert create_pet_response.status_code == 201
+def test_create_pet(pet_service,valid_pet_payload):
+    create_pet_response = pet_service.add_pet(valid_pet_payload)
+    assert create_pet_response.status_code == 200
 
     response_body = create_pet_response.json()
-    assert response_body.get("id") == ["id"]
+    assert response_body.get("id") == valid_pet_payload["id"]
+    assert response_body.get("name") == valid_pet_payload["name"]
+    assert response_body.get("description") == valid_pet_payload["description"]
 
--- Tests/conftest.py
+++ Tests/conftest.py
@@ -5,16 +5,10 @@ from Services import PetService
 
 @pytest.fixture(scope="session")
 def app_config():
-    """
-    Provides read-only access to application settings.
-    """
     return config
 
 @pytest.fixture(scope="session")
 def pet_service(app_config):
-    """
-    Session-scoped Pet API service.
-    """
     return PetService(
         base_url=app_config.base_url,
         headers=app_config.headers,
@@ -22,30 +16,23 @@ def pet_service(app_config):
     )
 
 @pytest.fixture
-def created_pet(pet_service, valid_pet_payload):
+def valid_pet_payload():
+    """Provides a valid pet payload."""
+    return pet_payload_factory()
 
-    # Create
+@pytest.fixture
+def sold_pet_payload():
+    """Provides a valid pet payload with status 'sold'."""
+    return pet_payload_factory(status="sold")
+
+@pytest.fixture
+def created_pet(pet_service, valid_pet_payload):
+    """Creates and cleans up a pet resource."""
     create_response = pet_service.add_pet(valid_pet_payload)
     pet_data = create_response.json()
-
     pet_id = pet_data["id"]
 
     yield pet_data
 
     # Cleanup
     pet_service.delete_pet(pet_id)
-
-    @pytest.fixture
-    def valid_pet_payload():
-        """
-        Provides a valid pet payload.
-        """
-        return pet_payload_factory()
-
-    @pytest.fixture
-    def sold_pet_payload():
-        """
-        Provides a valid pet payload with status 'sold'.
-        """
-        return pet_payload_factory(status="sold")
-
