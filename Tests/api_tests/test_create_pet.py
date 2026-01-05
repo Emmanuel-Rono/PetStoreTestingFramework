4d5790bab26f8c473933931efa5ff58eb95819c9
Emmanuel_Rono <ronoemmanuel933@gmail.com>
2026-01-05 22:04:10 +0300

    Adding assertions

--- Tests/api_tests/test_create_pet.py
+++ Tests/api_tests/test_create_pet.py
@@ -16,11 +16,11 @@ def test_create_pet(pet_service,valid_pet_payload):
 
 
     assert "tags" in response_body
+    assert isinstance(response_body["tags"], list)
     assert response_body["tags"][0]["id"] == valid_pet_payload["tags"][0]["id"]
     assert response_body["tags"][0]["name"] == valid_pet_payload["tags"][0]["name"]
 
-
+    #Assertions for Category
     assert "category" in response_body
-    assert isinstance(response_body["tags"], list)
     assert response_body["category"]["id"] == valid_pet_payload["category"]["id"]
     assert response_body["category"]["name"] == valid_pet_payload["category"]["name"]
