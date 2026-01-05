40bffed778fae7df7aa943629cb6310eabc93642
Emmanuel_Rono <ronoemmanuel933@gmail.com>
2026-01-05 22:02:55 +0300

    Adding assertions

--- Tests/api_tests/test_create_pet.py
+++ Tests/api_tests/test_create_pet.py
@@ -1,4 +1,5 @@
 import pytest
+from unicodedata import category
 
 from Tests.conftest import pet_service, valid_pet_payload
 
@@ -10,5 +11,16 @@ def test_create_pet(pet_service,valid_pet_payload):
     response_body = create_pet_response.json()
     assert response_body.get("id") == valid_pet_payload["id"]
     assert response_body.get("name") == valid_pet_payload["name"]
-    assert response_body.get("description") == valid_pet_payload["description"]
+    assert response_body.get("photoUrls") == valid_pet_payload["photoUrls"]
+    assert response_body.get("tags") == valid_pet_payload["tags"]
 
+
+    assert "tags" in response_body
+    assert response_body["tags"][0]["id"] == valid_pet_payload["tags"][0]["id"]
+    assert response_body["tags"][0]["name"] == valid_pet_payload["tags"][0]["name"]
+
+
+    assert "category" in response_body
+    assert isinstance(response_body["tags"], list)
+    assert response_body["category"]["id"] == valid_pet_payload["category"]["id"]
+    assert response_body["category"]["name"] == valid_pet_payload["category"]["name"]
